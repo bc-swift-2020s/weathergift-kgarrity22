c1b78dc77526bde797690a21df1da94a5feaae8a
Kathleen Garrity <garrityk@bc.edu>
2020-03-19 15:50:52 -0400

    updated with gitignore included

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,14 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Kathleen Garrity on 3/19/20.
+//  Copyright © 2020 Kathleen Garrity. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyCpyx07x5p7QpDOuyuGljAEnHGqiYXAitU"
+}
+
